c093662080392fcb25823cd15b4313b33b459cc0
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-16 15:45:06 +0900

    add: 手順1の実装

--- app/src/main/java/app/doggy/checkworkcount_customclass/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_customclass/MainActivity.kt
@@ -12,5 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        // 手順1
+        // Counter1クラスをインスタンス化
+        val counter1 = Counter1()
+
+        // plus_buttonをクリックした時の処理
+        binding.plusButton.setOnClickListener {
+            // numberプロパティの値を1増やす
+            counter1.number += 1
+
+            // numberプロパティの値をcount_textに表示する
+            binding.countText.text = counter1.number.toString()
+        }
     }
 }
